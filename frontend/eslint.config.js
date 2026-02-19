d3ac3b2261db0d795dc6e8e0ca78c357084984ea
Mohamed Rodani <rodani484@gmail.com>
2026-02-19 01:12:51 +0000

    Remove hard @eslint/js dependency from frontend lint config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,9 @@
-import js from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 import reactHooks from 'eslint-plugin-react-hooks';
 import localRules from './eslint-rules/index.js';
 
 export default [
-  js.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     ignores: ['node_modules/**', 'dist/**', 'build/**'],
